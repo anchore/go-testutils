6dea1542b4ffb400b08360a28a795358c135bb2c
Alex Goodman <wagoodman@users.noreply.github.com>
2020-09-22 15:46:07 -0400

    add OCI image fixture optoins (#4)
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- image_fixtures.go
+++ image_fixtures.go
@@ -16,7 +16,7 @@ import (
 
 var (
 	testFixturesDir = "test-fixtures"
-	tarCacheDir     = path.Join(testFixturesDir, "tar-cache")
+	cacheDir        = path.Join(testFixturesDir, "cache")
 	imagePrefix     = "anchore-fixture"
 )
 
@@ -31,6 +31,20 @@ func GetFixtureImage(t *testing.T, source, name string) (*image.Image, func()) {
 		location = GetFixtureImageTarPath(t, name)
 	case image.DockerDaemonSource:
 		location = LoadFixtureImageIntoDocker(t, name)
+	case image.OciTarballSource:
+		dockerArchivePath := GetFixtureImageTarPath(t, name)
+		ociArchivePath := path.Join(path.Dir(dockerArchivePath), "oci-archive-"+path.Base(dockerArchivePath))
+		if _, err := os.Stat(ociArchivePath); os.IsNotExist(err) {
+			skopeoCopyDockerArchiveToPath(t, dockerArchivePath, fmt.Sprintf("oci-archive:%s", ociArchivePath))
+		}
+		location = ociArchivePath
+	case image.OciDirectorySource:
+		dockerArchivePath := GetFixtureImageTarPath(t, name)
+		ociDirPath := path.Join(path.Dir(dockerArchivePath), "oci-dir-"+path.Base(dockerArchivePath))
+		if _, err := os.Stat(ociDirPath); os.IsNotExist(err) {
+			skopeoCopyDockerArchiveToPath(t, dockerArchivePath, fmt.Sprintf("oci:%s", ociDirPath))
+		}
+		location = ociDirPath
 	default:
 		t.Fatalf("could not determine source: %+v", source)
 	}
@@ -42,6 +56,28 @@ func GetFixtureImage(t *testing.T, source, name string) (*image.Image, func()) {
 	}
 
 	return i, stereoscope.Cleanup
+}
+
+func isSkopeoAvailable() bool {
+	_, err := exec.LookPath("skopeo")
+	return err == nil
+}
+
+func skopeoCopyDockerArchiveToPath(t *testing.T, dockerArchivePath, destination string) {
+	if !isSkopeoAvailable() {
+		t.Fatalf("cannot find skopeo executable")
+	}
+
+	cmd := exec.Command("skopeo", "copy", fmt.Sprintf("docker-archive:%s", dockerArchivePath), destination)
+	cmd.Env = os.Environ()
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("skopeo failed: %+v", err)
+	}
 }
 
 func getFixtureImageFromTar(t *testing.T, tarPath string) *image.Image {
@@ -86,11 +122,11 @@ func getFixtureImageTarPath(t *testing.T, fixtureName, tarStoreDir, tarFileName 
 	fullImageName := fmt.Sprintf("%s:%s", imageName, imageVersion)
 	tarPath := path.Join(tarStoreDir, tarFileName)
 
-	// create the tar-cache dir if it does not already exist...
-	if !fileOrDirExists(t, tarCacheDir) {
-		err := os.Mkdir(tarCacheDir, 0o755)
+	// create the cache dir if it does not already exist...
+	if !fileOrDirExists(t, cacheDir) {
+		err := os.Mkdir(cacheDir, 0o755)
 		if err != nil {
-			t.Fatalf("could not create tar cache dir (%s): %+v", tarCacheDir, err)
+			t.Fatalf("could not create tar cache dir (%s): %+v", cacheDir, err)
 		}
 	}
 
@@ -117,7 +153,7 @@ func GetFixtureImageTarPath(t *testing.T, name string) string {
 	t.Helper()
 	imageName, imageVersion := getFixtureImageInfo(t, name)
 	tarFileName := fmt.Sprintf("%s-%s.tar", imageName, imageVersion)
-	return getFixtureImageTarPath(t, name, tarCacheDir, tarFileName)
+	return getFixtureImageTarPath(t, name, cacheDir, tarFileName)
 }
 
 func fixtureVersion(t *testing.T, name string) string {
